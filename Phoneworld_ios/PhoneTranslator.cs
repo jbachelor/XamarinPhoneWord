dd1c7ede8ea8dfd78cb4cf97a6edbc0c9b48d6a6
Jonathan Bachelor <jon_bachelor@me.com>
2016-08-19 13:05:57 -0700

    Very basic 'hello iOS' application from Xamarin tutorial:  https://developer.xamarin.com/guides/ios/getting_started/hello,_iOS/hello,iOS_quickstart/

---
+++ Phoneworld_ios/PhoneTranslator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+
+namespace Phoneworld_ios
+{
+	public static class PhoneTranslator
+	{
+		public static string ToNumber(string raw)
+		{
+			if (string.IsNullOrWhiteSpace(raw))
+			{
+				return string.Empty;
+			}
+
+			raw = raw.ToUpperInvariant();
+
+			var newNumberSB = new StringBuilder();
+			foreach (var c in raw)
+			{
+				if (" -0123456789".Contains(c))
+				{
+					newNumberSB.Append(c);
+				}
+				else
+				{
+					var result = TranslateToNumber(c);
+					if (result != null)
+					{
+						newNumberSB.Append(result);
+					}
+				}
+			}
+
+			return newNumberSB.ToString();
+		}
+
+		public static bool Contains(this string keyString, char c)
+		{
+			return keyString.IndexOf(c) >= 0;
+		}
+
+
+		public static int? TranslateToNumber(char digit)
+		{
+			int? result = null;
+
+			if ("ABC".Contains(digit))
+			{
+				result = 2;
+			}
+			else if ("DEF".Contains(digit))
+			{
+				result = 3;
+			}
+			else if ("GHI".Contains(digit))
+			{
+				result = 4;
+			}
+			else if ("JKL".Contains(digit))
+			{
+				result = 5;
+			}
+			else if ("MNO".Contains(digit))
+			{
+				result = 6;
+			}
+			else if ("PQRS".Contains(digit))
+			{
+				result = 7;
+			}
+			else if ("TUV".Contains(digit))
+			{
+				result = 8;
+			}
+			else if ("WXYZ".Contains(digit))
+			{
+				result = 9;
+			}
+
+			return result;
+		}
+	}
+}
+
---
+++ Phoneworld_ios/ViewController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UIKit;
+using Foundation;
+
+
+namespace Phoneworld_ios
+{
+	public partial class ViewController : UIViewController
+	{
+		private string translatedNumber;
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			ConfigureTranslatedNumberButton();
+			ConfigureCallButton();
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		private void ConfigureCallButton()
+		{
+			CallButton.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				var url = new NSUrl($"tel:{translatedNumber}");
+
+				if (!UIApplication.SharedApplication.OpenUrl(url))
+				{
+					ShowOkAlertDialog("Sorry...",
+									  "The scheme 'tel:' is not supported on this device.");
+				}
+			};
+		}
+
+		private void ShowOkAlertDialog(string title = "Alert", string message = "Sorry, unable to perform this action")
+		{
+			var alert = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+			alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+			PresentViewController(alert, true, null);
+		}
+
+		private void ConfigureTranslatedNumberButton()
+		{
+			translatedNumber = string.Empty;
+			TranslateButton.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				PhoneNumberText.ResignFirstResponder();
+				translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+
+				if (string.IsNullOrWhiteSpace(translatedNumber))
+				{
+					CallButton.SetTitle("Call", UIControlState.Normal);
+					CallButton.Enabled = false;
+				}
+				else
+				{
+					CallButton.SetTitle($"Call {translatedNumber}", UIControlState.Normal);
+					CallButton.Enabled = true;
+				}
+			};
+		}
+	}
+}
+
